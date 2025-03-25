00e7b2004d08ae9f609e982ce3995740fbe638d3
JongHyun070105 <bobi09361@gmail.com>
2025-03-25 13:50:18 +0900

    Sebasi_python

---
+++ Day0325/03.py
@@ -0,0 +1,5 @@
+print("Let\'s do it")
+print(""" -," ' """)
+print("\\")
+print("1\n2")
+print("1\t2")
---
+++ Day0325/04.py
@@ -0,0 +1,8 @@
+x, y = 1, 2
+
+if x > y :
+    print(x)
+elif x == y :
+    print(x,'=',y)
+else :
+    print(y)

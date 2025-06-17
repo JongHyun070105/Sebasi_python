ba03c175d1cf42625d49b8271fe10788a5665015
JongHyun070105 <bobi09361@gmail.com>
2025-06-17 13:46:27 +0900

    Sebasi_python

---
+++ Day0617/03.py
@@ -0,0 +1,13 @@
+while True:
+    a,b,c = map(int, input().split())
+
+
+    if a == 0 & b == 0 & c == 0:
+        print("wrong")
+        break
+    else:
+        if((a * a) + (b * b) == (c * c) or (b * b) + (c * c) == (a * a) or (a * a) +(c * c) == (b * b)):
+            print("right")
+        else:
+            print("wrong")
+
